963e4f54e39a8c5d19231111889cabd835a98040
rmechany <royy1230@gmail.com>
2024-02-25 11:35:56 +0200

    1

---
+++ angular/services.ts
@@ -0,0 +1,3 @@
+export { AccountSettingsService } from './services/account-settings.service';
+export { RulesService } from './services/rules.service';
+export { SensorsService } from './services/sensors.service';
